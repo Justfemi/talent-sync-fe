06485c27d4bfc7adad2c0cf00590fa01f37d5e33
Justfemi <ajibade.olufemi@yahoo.com>
2024-01-24 17:40:39 +0100

    bug: new button fix

--- src/components/whychoose.js
+++ src/components/whychoose.js
@@ -17,7 +17,7 @@ const Whychoose = () => {
       </p>
 
       <img 
-        className="absolute right-[300px] bottom-[-642px] w-[200px]"
+        className="absolute right-[29git 0px] bottom-[-645px] w-[200px]"
         src={arrow}
         alt="curly arrow"
       />
